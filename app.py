80bc0d9eaf72ea1f0ac062b7575d6e793dde21f0
Robert W Schell <schell.rw@gmail.com>
2023-09-05 17:14:17 -0500

    parenthetically err

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.title("Redfin U.S. Real Estate Heatmap")
 
 #st.markdown("## Competitive intelligence and easy-to-use technology platforms are just a click away.") # Hover over the map to view more details.")
 ####st.subheader("Your company's information paired with our market research and technology solutions are exaclty what you've been looking for.") ##] :red[exaclty what you've been looking for.]")
-st.subheader("Actionable insights and opportunities for your business to :red[Pre Dominate the Competition]"), divider="rainbow")  #:blue[the competition.]", divider="rainbow")
+st.subheader("Actionable insights and opportunities for your business to :red[Pre Dominate the Competition]", divider="rainbow")  #:blue[the competition.]", divider="rainbow")
 #st.subheader(":red[Pre-Dominantly]", divider="rainbow")
 # st.subheader('''
 #              :orange[### *What markets are you in?  What metrics matter to you?  How do you want your business to grow?  How far do you want to go?*]''')
